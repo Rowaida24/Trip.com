e21f81204c445c003b62d9ca2299066802293ad7
Rowaida <99667649+Rowaida24@users.noreply.github.com>
2024-02-06 00:00:10 +0200

    Add files via upload

---
+++ book_db.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2022 at 05:17 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `book_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_form`
+--
+
+CREATE TABLE `book_form` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `guests` int(255) NOT NULL,
+  `arrivals` date NOT NULL,
+  `leaving` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book_form`
+--
+
+INSERT INTO `book_form` (`id`, `name`, `email`, `phone`, `address`, `location`, `guests`, `arrivals`, `leaving`) VALUES
+(1, 'rowaida', 'rwaida.omran@gmail.com', '0112610110', 'cairo', 'london', 5, '2022-12-20', '2022-12-31'),
+(2, 'rowaida', 'rwaida.omran@gmail.com', '0112610110', 'cairo', 'london', 7, '2022-12-19', '2022-12-28'),
+(3, '', '', '', '', '', 0, '0000-00-00', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_form`
+--
+
+CREATE TABLE `user_form` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_form`
+--
+
+INSERT INTO `user_form` (`id`, `name`, `email`, `password`) VALUES
+(1, 'rowaida', 'rwaida.omran@gmail.com', 'e10adc3949ba59abbe56e057f20f883e');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_form`
+--
+ALTER TABLE `book_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_form`
+--
+ALTER TABLE `user_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_form`
+--
+ALTER TABLE `book_form`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user_form`
+--
+ALTER TABLE `user_form`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ register.php
@@ -0,0 +1,153 @@
+<?php
+
+include 'config.php';
+
+if (isset($_POST['submit'])) {
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+
+   $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if (mysqli_num_rows($select) > 0) {
+      $message[] = 'user already exist!';
+   } else {
+      mysqli_query($conn, "INSERT INTO `user_form`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
+      $message[] = 'registered successfully!';
+      header('location:login.php');
+   }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Trip.com</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+
+   <?php
+   if (isset($message)) {
+      foreach ($message as $message) {
+         echo '<div class="message" onclick="this.remove();">' . $message . '</div>';
+      }
+   }
+   ?>
+
+   <!-- header section starts  -->
+
+   <section class="header">
+      <a href="home.html" class="logo">Trip.com</a>
+      <form action="" class="search-form">
+         <input type="search" id="search-box" placeholder="search here..." />
+         <label for="search-box" class="fas fa-search"></label>
+      </form>
+      <nav class="navbar">
+         <a href="home.html">home</a>
+         <a href="about.html">about</a>
+         <a href="package.html">package</a>
+         <a href="login.php">book</a>
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+   </section>
+
+   <!-- header section ends -->
+
+   <div class="heading" style="background: url(images/header-bg-2.jpg) no-repeat">
+      <h1>book now</h1>
+   </div>
+
+   <section class="booking">
+      <h1 class="heading-title">register</h1>
+      <form action="" method="post" class="book-form">
+         <div class="flex">
+            <div class="inputBox">
+               <span>name :</span>
+               <input type="text" name="name" required placeholder="enter your name">
+            </div>
+            <div class="inputBox">
+               <span>email :</span>
+               <input type="email" name="email" required placeholder="enter your email">
+            </div>
+            <div class="inputBox">
+               <span>password :</span>
+               <input type="password" name="password" required placeholder="enter password">
+            </div>
+            <div class="inputBox">
+               <span>confirm password :</span>
+               <input type="password" name="cpassword" required placeholder="confirm password">
+            </div>
+         </div>
+         <input type="submit" name="submit" class="btn" value="register now">
+         <p>already have an account? <a href="login.php">login now</a></p>
+      </form>
+   </section>
+
+   <section class="footer">
+      <div class="box-container">
+         <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         </div>
+
+         <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> Your trip@gmail.com </a>
+            <a href="#"> <i class="fas fa-map"></i> Cairo, Egypt - 12345 </a>
+         </div>
+
+         <div class="box">
+            <h3>quick links</h3>
+            <a href="home.html"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.html"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.html"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="login.php"> <i class="fas fa-angle-right"></i> book</a>
+         </div>
+
+         <div class="box">
+            <h3>extra links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+         </div>
+
+
+         <div class="credit">
+            created by <span>Eng.Rowaida Tamer Mohamed </span> 2022.
+         </div>
+   </section>
+
+   <!-- footer section ends -->
+
+   <!-- swiper js link  -->
+   <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+</body>
+
+</html>
